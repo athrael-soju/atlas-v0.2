b121c693f99a295924f25985637c8a4a8ba1c19b
Athrael Soju <athrael.soju@gmail.com>
2024-08-27 01:08:37 +0100

    refactor file handling

--- components/knowledgebase/index.tsx
+++ components/knowledgebase/index.tsx
@@ -1,49 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useUploadFile } from '@/hooks/use-upload-file';
+import { useHandleFiles } from '@/hooks/use-handle-files';
 import { FileUploader } from '@/components/file-uploader';
 import { UploadedFiles } from './uploaded-files';
-import { toast } from '@/components/ui/use-toast';
-
-const getUploadedFiles = async () => {
-  try {
-    const response = await fetch('/api/uploadthing', {
-      method: 'GET'
-    });
-    const data = await response.json();
-    if (!response.ok) {
-      toast({
-        title: 'Uh oh! Something went wrong.',
-        description: data.error || 'Failed to retrieve file list',
-        variant: 'destructive'
-      });
-      return [];
-    }
-    return data;
-  } catch (error) {
-    toast({
-      title: 'Uh oh! Something went wrong.',
-      description: `${error}`,
-      variant: 'destructive'
-    });
-    return [];
-  }
-};
 
 export function Knowledgebase() {
   const { onUpload, progresses, uploadedFiles, setUploadedFiles, isUploading } =
-    useUploadFile('attachment', { defaultUploadedFiles: [] });
+    useHandleFiles('attachment');
 
-  useEffect(() => {
-    const fetchFiles = async () => {
-      const list = await getUploadedFiles();
-      if (list.files.length > 0) {
-        setUploadedFiles(list.files);
-      }
-    };
-    fetchFiles();
-  }, [setUploadedFiles]); // Ensure this effect runs only once on mount
   return (
     <div className="space-y-6">
       <FileUploader
