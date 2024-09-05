6d5349f2d65461ad4c70e11721f6479517b9ec3d
Athrael Soju <athrael.soju@gmail.com>
2024-09-05 12:55:04 +0100

    Refactor mobile sidebar component to import and use SheetTitle and SheetDescription components

--- components/layout/mobile-sidebar.tsx
+++ components/layout/mobile-sidebar.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { DashboardNav } from '@/components/dashboard-nav';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription
+} from '@/components/ui/sheet'; // import SheetTitle
 import { NavItem } from '@/types';
 import { MenuIcon } from 'lucide-react';
 import { useState } from 'react';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'; // import VisuallyHidden
 
-// import { Playlist } from "../data/playlists";
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  // playlists: Playlist[];
   navItems: NavItem[];
 }
 
@@ -20,6 +25,10 @@ export function MobileSidebar({ navItems }: SidebarProps) {
           <MenuIcon />
         </SheetTrigger>
         <SheetContent side="left" className="!px-0">
+          <VisuallyHidden>
+            <SheetTitle />
+            <SheetDescription />
+          </VisuallyHidden>
           <div className="space-y-4 py-4">
             <div className="px-3 py-2">
               <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
