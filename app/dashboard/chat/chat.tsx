7a3c8601f9e0687961382f10123d427c298eaab8
Athrael Soju <athrael.soju@gmail.com>
2024-09-13 16:39:01 +0100

    refactor: Enhance Chat component layout and input handling

--- app/dashboard/chat/chat.tsx
+++ app/dashboard/chat/chat.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Spinner } from '@/components/spinner';
@@ -110,7 +111,7 @@ const Chat = () => {
   return (
     <TooltipProvider>
       <div
-        className="relative flex h-full min-h-[50vh] flex-col rounded-xl  bg-muted/50 p-4 lg:col-span-2"
+        className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2 "
         style={{ height: 'calc(100vh - 185px)' }}
       >
         <div className="mb-4 flex-1 space-y-4 overflow-y-auto px-4">
@@ -119,11 +120,16 @@ const Chat = () => {
           ))}
           {isThinking && <Spinner />}
           <div ref={userInputRef} />
+          {/* <div className="flex h-full items-center justify-center">
+            <div className="flex h-full items-center justify-center">
+              <Image src="/atlas.png" alt="Chatting" width={100} height={100} />
+            </div>
+          </div> */}
         </div>
 
         <form
           onSubmit={handleSubmit}
-          className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
+          className="relative mx-auto w-full max-w-[600px] overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
         >
           <Label htmlFor="message" className="sr-only">
             Message
@@ -132,9 +138,10 @@ const Chat = () => {
             id="message"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
-            placeholder="Type your message here..."
-            className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+            placeholder="Send a message..."
+            className="min-h-12 w-full resize-none border-0 p-3 shadow-none focus-visible:ring-0"
             disabled={inputDisabled}
+            rows={1}
           />
           <div className="flex items-center p-3 pt-0">
             <Tooltip>
